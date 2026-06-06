a6d31d48ffaa42560f9ee901b732caf6a60b0be8
kikson9 <tochukwuokike7@gmail.com>
2026-06-06 13:27:59 +0800

    added particle class for particle debris feature

--- missile_commander.cpp
+++ missile_commander.cpp
@@ -280,6 +280,56 @@ public:
     }
 };
 
+class Particle
+{
+public:
+    Vector2 position;
+    Vector2 velocity;
+    float alpha;
+    float size;
+    bool active;
+
+    Particle()
+    {
+        position = {0, 0};
+        velocity = {0, 0};
+        alpha = 0.0f;
+        size = 0.0f;
+        active = false;
+    }
+
+    void Reset()
+    {
+        active = false;
+        alpha = 0.0f;
+    }
+
+    void Update()
+    {
+        if (!active)
+            return;
+
+        // Move by velocity each frame
+        position.x += velocity.x;
+        position.y += velocity.y;
+
+        // Fade out
+        alpha -= 8.0f;
+
+        // Deactivate when fully invisible
+        if (alpha <= 0)
+            Reset();
+    }
+
+    void Draw()
+    {
+        if (!active)
+            return;
+        DrawCircleV(position, size,
+                    (Color){255, 200, 50, (unsigned char)alpha});
+    }
+};
+
 //------------------------------------------------------------------------------------
 // Game Class
 //------------------------------------------------------------------------------------
